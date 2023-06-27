29216933a07127d98ea8650b8f439ce0903a1605
okelly54 <okelly@qa.com>
2023-06-27 16:35:06 +0100

    in progress database stuff

--- TaxiBookingSystem/src/main/java/com/qa/tbs/service/customer/CustomerService.java
+++ TaxiBookingSystem/src/main/java/com/qa/tbs/service/customer/CustomerService.java
@@ -10,13 +10,4 @@ public interface CustomerService {
     Customer getById(int id);
     List<Customer> getAll();
     Customer update(int id, String firstName, String surname, Date dob, String address1, String address2, String postcode, String number);
-
-    //    private Integer customerId;
-    //    private String firstName;
-    //    private String surname;
-    //    private Date dob;
-    //    private String address1;
-    //    private String address2;
-    //    private String postcode;
-    //    private String number;
 }
